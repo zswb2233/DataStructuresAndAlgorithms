4337f291faaed270782966aecaf1af4d3706cfbd
itcast <hello@itcast.cn>
2025-04-13 21:08:20 +0800

    二分查找（全）

--- algorithms/src/BinarySearch.java
+++ algorithms/src/BinarySearch.java
@@ -73,4 +73,52 @@ public class BinarySearch {
             return -1;
         }
     }
+
+    /**
+     * 当有多个位置的有序数组满足条件，要求最左边的情况，右边是同理。
+     * @param a
+     * @param target
+     * @return
+     */
+    public static int binarySearchLeftmost1(int[] a, int target) {
+        int i=0;
+        int j=a.length-1;//记录指针和初值
+        int candidate=-1;//候选者
+        //范围内有东西
+        while(i<=j) {
+            int m=(i+j)>>1;
+            //不是(i+j)/2，因为可能超过最大值。
+            //32为，int最大值为2^31-1，最高位是符号位
+            if(target<a[m]){
+                j=m-1;
+            }else if(a[m]<target) {
+                i = m + 1;
+            }else{
+                //找到了
+                //记录候选位置
+                candidate = m;
+                j=m-1;
+            }
+        }
+        return candidate;//表示找不到
+    }
+
+    /**
+     * 返回<=target 的最靠右的索引
+     * @param a
+     * @param target
+     * @return
+     */
+    public static int binarySearchRightmost2(int[] a, int target) {
+        int i=0,j=a.length-1;
+        while(i<=j){
+            int m=(i+j)>>1;
+            if(target<a[m]){
+                j=m-1;
+            }else{
+                i=m+1;
+            }
+        }
+        return i-1;
+    }
 }
