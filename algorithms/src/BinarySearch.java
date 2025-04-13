2fec78b0bffbd88d2aff32844412e9c71c664e35
itcast <hello@itcast.cn>
2025-04-13 16:26:41 +0800

    二分查找（基础，提升，最终提升（平衡））

---
+++ algorithms/src/BinarySearch.java
@@ -0,0 +1,76 @@
+public class BinarySearch {
+    /**
+     * 二分查找基础版
+     * 要求：有序序列
+     */
+
+    public static int binarySearchBasic(int[] a, int target) {
+        int i=0;
+        int j=a.length-1;//记录指针和初值
+        //范围内有东西
+        while(i<=j) {
+            int m=(i+j)>>1;
+            //不是(i+j)/2，因为可能超过最大值。
+            //32为，int最大值为2^31-1，最高位是符号位
+            if(target<a[m]){
+                j=m-1;
+            }else if(a[m]<target) {
+                i = m + 1;
+            }else{
+                //找到了
+                return m;
+            }
+        }
+        return -1;//表示找不到
+    }
+
+    /**
+     * j不作为边界,少一次减法运算。
+     * @param a
+     * @param target
+     * @return
+     */
+    public static int binarySearchUpper(int[] a, int target) {
+        int i=0;
+        int j=a.length;//作为边界值
+        //范围内有东西
+        while(i<j) {
+            int m=(i+j)>>1;
+            //不是(i+j)/2，因为可能超过最大值。
+            //32为，int最大值为2^31-1，最高位是符号位
+            if(target<a[m]){
+                j=m;
+            }else if(a[m]<target) {
+                i = m + 1;
+            }else{
+                //找到了
+                return m;
+            }
+        }
+        return -1;//表示找不到
+    }
+
+    /**
+     * 缩小边界
+     * @param a
+     * @param target
+     * @return
+     */
+    public static int binarySearch3(int[] a, int target) {
+        int i=0, j=a.length;
+        while(j-i>1){
+            int m=(i+j)>>1;
+            if(target<a[m]){
+                j=m;
+            }
+            else{
+                i=m;
+            }
+        }
+        if(a[i]==target){
+            return i;
+        }else {
+            return -1;
+        }
+    }
+}
